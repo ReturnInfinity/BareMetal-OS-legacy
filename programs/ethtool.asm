52af55379a6a62886e8a05afcb6019bd6bdd4da1
Ian Seyler <ian.seyler@returninfinity.com>
2013-05-13 14:12:26 -0400

    Update ethtool to use new API format

--- programs/ethtool.asm
+++ programs/ethtool.asm
@@ -12,10 +12,10 @@
 ethtool:
 
 	mov rsi, startstring
-	call b_print_string
+	call b_output
 
 ethtool_command:
-	call b_input_key_wait
+	call b_input_key
 	or al, 00100000b			; Convert character to lowercase if it is not already
 
 	cmp al, 's'
@@ -27,46 +27,49 @@ ethtool_command:
 	jmp ethtool_command			; Didn't get any key we were expecting so try again.
 
 ethtool_finish:
-	call b_print_newline
+	mov rsi, endstring
+	call b_output
 	ret					; Back to OS
 
 ethtool_send:
 	mov rsi, sendstring
-	call b_print_string
-	mov rdi, broadcastaddress
-	mov rsi, startstring
-	mov rbx, 0xABBA
-	mov rcx, 63
+	call b_output
+	mov rsi, packet
+	mov rcx, 1500
 	call b_ethernet_tx
 	mov rsi, sentstring
-	call b_print_string
+	call b_output
 	jmp ethtool_command
 	
 ethtool_receive:
 	mov rsi, receivestring
-	call b_print_string
+	call b_output
 	mov rdi, EthernetBuffer
 	call b_ethernet_rx
 	cmp rcx, 0
 	je ethtool_receive_nopacket
 	mov rsi, receiveddata
-	call b_print_string
-	mov rsi, EthernetBuffer
-	call b_debug_dump_mem
+	call b_output
+;	mov rsi, EthernetBuffer
+;	call b_debug_dump_mem
 	jmp ethtool_command
 
 ethtool_receive_nopacket:
 	mov rsi, receivednothingstring
-	call b_print_string
+	call b_output
 	jmp ethtool_command
 
 ; -----------------------------------------------------------------
 
+packet:
+destination: db 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF
+source: db 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+ethertype: db 0xAB, 0xBA
 startstring: db 'EthTool: S to send a packet, R to recieve a packet, Q to quit.', 0
+endstring: db 13, 0
 sendstring: db 13, 'Sending packet, ', 0
 sentstring: db 'Sent', 0
 receivestring: db 13, 'Receiving packet, ', 0
 receivednothingstring: db 'Nothing there', 0
 receiveddata: db 'Data received', 13, 0
-broadcastaddress: db 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0
 EthernetBuffer: db 0
