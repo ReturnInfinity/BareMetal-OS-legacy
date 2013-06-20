844d7e9737d4e1aaeb8d716ef4601c12e0d787af
Ian Seyler <ian.seyler@returninfinity.com>
2013-06-20 14:58:32 -0400

    Remove build scripts and binaries

--- build.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-REM This script assumes that 'nasm' is in your path
-
-cd os
-echo | set /p x=Building BareMetal OS... 
-call nasm kernel64.asm -o ..\kernel64.sys && (echo Success) || (echo Error!)
-cd ..
-pause
--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-cd os
-nasm kernel64.asm -o ../kernel64.sys
