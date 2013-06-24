48036796de740291d9fe547f39d31dbbe429309f
Ian Seyler <ian.seyler@returninfinity.com>
2013-06-24 16:43:30 -0400

    Add time demo to newlib test

--- newlib/test.c
+++ newlib/test.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
+#include <time.h>
 
 char tempstring[32];
 
 int main()
 {
+	struct tm *local;
+	time_t t;
+
 	printf("NewLib Test Application\n=======================\n");
+	t = time(NULL);
+	local = localtime(&t);
+	printf("Local time and date: %s\n", asctime(local));
 	printf("%s %d\n", "Output:", 1234);
 	printf("Enter some text: ");
 	fgets(tempstring, 32, stdin);			// Get up to 32 chars from the keyboard
