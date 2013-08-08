1db76f87e53a7f97dcfd7ea6d90e77a656c93313
Ian Seyler <ian.seyler@returninfinity.com>
2013-08-08 11:03:09 -0400

    os_pixel_put working in 24 bit mode

--- os/syscalls/graphics.asm
+++ os/syscalls/graphics.asm
@@ -12,10 +12,50 @@ align 16
 
 ; -----------------------------------------------------------------------------
 ; os_pixel_put -- Put a pixel on the screen
-;  IN:	AH  = row
-;	AL  = column
+;  IN:	EBX = Packed X & Y coordinates (YYYYXXXX)
+;	EAX = Pixel Details (AARRGGBB)
 ; OUT:	All registers preserved
 os_pixel_put:
+	cmp byte [os_VideoDepth], 32
+	je os_pixel_put_32
+	cmp byte [os_VideoDepth], 24
+	je os_pixel_put_24
+	ret
+os_pixel_put_32:
+os_pixel_put_24:
+	push rdi
+	push rcx
+	push rbx
+	push rax
+
+	push rax
+	mov rax, rbx
+	shr eax, 16
+	xor ecx, ecx
+	mov cx, [os_VideoX]
+	mul ecx
+	and ebx, 0x0000FFFF
+	add eax, ebx
+	mov ecx, 3
+	mul ecx
+	mov rdi, [os_VideoBase]
+	add rdi, rax
+	pop rax
+
+; multiply Y by os_VideoX
+; add X
+; multiply by 3 or 4
+
+	stosb
+	shr eax, 8
+	stosb
+	shr eax, 8
+	stosb
+
+	pop rax
+	pop rbx
+	pop rcx
+	pop rdi
 	ret
 ; -----------------------------------------------------------------------------
 
