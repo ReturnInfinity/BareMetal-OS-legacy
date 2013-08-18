70e1472983dbb703208b2cc0e8517cabe88c094e
Ian Seyler <ian.seyler@returninfinity.com>
2013-08-17 20:16:09 -0400

    3dstars update

--- programs/3dstars.c
+++ programs/3dstars.c
@@ -5,6 +5,7 @@ ld -T app.ld -o 3dstars.app crt0.o 3dstars.o libBareMetal.o libc.a libm.a
 */
 
 #include <math.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "libBareMetal.h"
@@ -24,14 +25,20 @@ typedef struct {
 
 int main()
 {
-	int n, dx, dy;
-	double x, y, z, rx, ry, rz;
+	int n, running=1;
+	double x, y, z, rx, ry, rz, xoffset=0, yoffset=0, zoffset=0;
 	double rads = 360 / (2 * M_PI);
 	double sind1, cosd1, sind2, cosd2;
 	stardata star[numstars];
 	float a1=0, a2=0, zval=0;
+	unsigned char tchar;
 
 	VideoMemory = (char*)b_system_config(20, 0);
+	if (VideoMemory == 0)
+	{
+		printf("Video mode is required for this program.\n");
+		return 1;
+	}
 	VideoX = b_system_config(21, 0);
 	VideoY = b_system_config(22, 0);
 	VideoBPP = b_system_config(23, 0);
@@ -51,8 +58,17 @@ int main()
 
 	clear_screen();
 
-	while(1)
+	while(running==1)
 	{
+		tchar = b_input_key();
+
+		if (tchar == 'q')
+			running = 0;
+		else if (tchar = '-')
+			zoffset -= 1000;
+		else if (tchar = '=')
+			zoffset += 1000;
+
 		sind1 = sin(a1/rads);
 		cosd1 = cos(a1/rads);
 		sind2 = sin(a2/rads);
@@ -60,19 +76,17 @@ int main()
 
 		for (n=0; n<numstars; n++)
 		{
-			x = star[n].x;
-			y = star[n].y;
-			z = star[n].z;
+			x = star[n].x + xoffset;
+			y = star[n].y + yoffset;
+			z = star[n].z + zoffset;
 
 			rx = (x * cosd1) - (z * sind1);
 			rz = (x * sind1) + (z * cosd1);
 			ry = (y * cosd2) - (rz * sind2);
 			rz = (y * sind2) + (rz * cosd2);
 
-			zval = rz + 1000;
-
-			star[n].dx = (VideoX / 2) + ((PER * rx) / zval);
-			star[n].dy = (VideoY / 2) + ((PER * ry) / zval);
+			star[n].dx = (VideoX / 2) + ((PER * rx) / rz);
+			star[n].dy = (VideoY / 2) + ((PER * ry) / rz);
 		}
 		a1+=0.01;
 		a2+=0.00001;
@@ -85,6 +99,8 @@ int main()
 			star[n].dy1 = star[n].dy;
 		}
 	}
+
+	clear_screen();
 }
 
 void clear_screen()
