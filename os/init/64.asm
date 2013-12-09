9e27b6850eb220d6f3efcab7870b9bc74259d474
Ian Seyler <ian.seyler@returninfinity.com>
2013-12-09 12:29:12 -0500

    Comment for last change
    For those who have not used LOOP before

--- os/init/64.asm
+++ os/init/64.asm
@@ -13,11 +13,11 @@ align 16
 init_64:
 	; Make sure that memory range 0x110000 - 0x200000 is cleared
 	mov rdi, os_SystemVariables
-	mov rcx, 122880 ; Clear 960 KiB
+	mov rcx, 122880            ; Clear 960 KiB
 	xor rax, rax
 clearmem:
 	stosq
-	loop clearmem
+	loop clearmem              ; Decrement RCX by 1 and jump if greater than 0
 
 	mov word [os_Screen_Rows], 25
 	mov word [os_Screen_Cols], 80
