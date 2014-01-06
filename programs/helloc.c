b6bc8dff8e772943406c8e87fd2a43fff187e234
Ian Seyler <ian.seyler@returninfinity.com>
2014-01-06 15:25:41 -0500

    Removed obsolete commands.

--- programs/helloc.c
+++ programs/helloc.c
@@ -6,15 +6,11 @@
 // GCC (Tested with 4.5.0)
 // gcc -c -m64 -nostdlib -nostartfiles -nodefaultlibs -fomit-frame-pointer -mno-red-zone -o helloc.o helloc.c
 // gcc -c -m64 -nostdlib -nostartfiles -nodefaultlibs -fomit-frame-pointer -mno-red-zone -o libBareMetal.o libBareMetal.c
-// objcopy --remove-section .eh_frame --remove-section .rel.eh_frame --remove-section .rela.eh_frame helloc.o
-// objcopy --remove-section .eh_frame --remove-section .rel.eh_frame --remove-section .rela.eh_frame libBareMetal.o
 // ld -T app.ld -o helloc.app helloc.o libBareMetal.o
 //
 // Clang (Tested with 2.7)
 // clang -c -mno-red-zone -o libBareMetal.o libBareMetal.c
 // clang -c -mno-red-zone -o helloc.o helloc.c
-// objcopy --remove-section .eh_frame --remove-section .rel.eh_frame --remove-section .rela.eh_frame helloc.o
-// objcopy --remove-section .eh_frame --remove-section .rel.eh_frame --remove-section .rela.eh_frame libBareMetal.o
 // ld -T app.ld -o helloc.app helloc.o libBareMetal.o
 
 
