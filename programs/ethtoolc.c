e5577cb1bf132ac7e41f9a1e9f87c8c324070bf1
Ian Seyler <ian.seyler@returninfinity.com>
2014-01-20 17:42:32 -0500

    rename C ethtool

--- programs/ethtoolc.c
+++ programs/ethtoolc.c
@@ -1,3 +1,7 @@
+// gcc -c -m64 -nostdlib -nostartfiles -nodefaultlibs -fomit-frame-pointer -mno-red-zone -o ethtoolc.o ethtoolc.c
+// gcc -c -m64 -nostdlib -nostartfiles -nodefaultlibs -fomit-frame-pointer -mno-red-zone -o libBareMetal.o libBareMetal.c
+// ld -T app.ld -o ethtoolc.app ethtoolc.o libBareMetal.o
+
 #include "libBareMetal.h"
 
 void ethtool_send();
