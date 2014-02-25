2ff38f737bdc7a688f4f762e4e6cebe227b584d0
Ian Seyler <ian.seyler@returninfinity.com>
2014-02-25 17:35:32 -0500

    ethtoolc.app callback fix

--- programs/ethtoolc.c
+++ programs/ethtoolc.c
@@ -1,11 +1,11 @@
-// gcc -c -m64 -nostdlib -nostartfiles -nodefaultlibs -fomit-frame-pointer -mno-red-zone -o ethtoolc.o ethtoolc.c
-// gcc -c -m64 -nostdlib -nostartfiles -nodefaultlibs -fomit-frame-pointer -mno-red-zone -o libBareMetal.o libBareMetal.c
+// gcc -c -m64 -nostdlib -nostartfiles -nodefaultlibs -o ethtoolc.o ethtoolc.c
+// gcc -c -m64 -nostdlib -nostartfiles -nodefaultlibs -o libBareMetal.o libBareMetal.c
 // ld -T app.ld -o ethtoolc.app ethtoolc.o libBareMetal.o
 
 #include "libBareMetal.h"
 
 void ethtool_send();
-//void ethtool_receive();
+void ethtool_receive();
 
 char packet[1500];
 
@@ -16,26 +16,24 @@ int main(void)
 {
 	b_output("EthTool: S to send a packet, Q to quit.\nReceived packets will display automatically.");
 	// Configure the network callback
-//	b_system_config(networkcallback_set, (unsigned long int)ethtool_receive);
+	b_system_config(networkcallback_set, (unsigned long int)ethtool_receive);
 
 	while (running == 1)
 	{
-		len = b_ethernet_rx(packet);
-		if (len != 0)
-		{
-			b_output("\nReceived packet\n");
-			b_system_misc(debug_dump_mem, packet, len);
-		}
 		key = b_input_key();
 		if (key == 's')
+		{
 			ethtool_send();
+		}
 		else if (key == 'q')
+		{
 			running = 0;
+		}
 	}
 
 	b_output("\n");
 	// Clear the network callback
-//	b_system_config(networkcallback_set, 0);
+	b_system_config(networkcallback_set, 0);
 
 	return 0;
 }
@@ -47,9 +45,9 @@ void ethtool_send()
 	b_output("Sent!");
 }
 
-//void ethtool_receive()
-//{
-//	b_output("\nReceived packet\n");
-//	len = b_ethernet_rx(packet);
-//	b_system_misc(debug_dump_mem, packet, len);
-//}
+void ethtool_receive()
+{
+	b_output("\nReceived packet\n");
+	len = b_ethernet_rx(packet);
+	b_system_misc(debug_dump_mem, packet, len);
+}
